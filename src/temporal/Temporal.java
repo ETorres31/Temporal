749468b313b9cbabb9ee03f28c05dc3ec99516b0
DELL <DELL@DELL-PC>
2018-04-11 20:41:40 -0600

    Este es un Simple Comentario Sobre lo Nuevo.

---
+++ src/temporal/Temporal.java
@@ -0,0 +1,16 @@
+package temporal;
+
+/**
+ * Nombre:
+ * Fecha: 11-04-2018
+ * Version: 1.0
+ * Copyright: Itca
+ * @author Erck
+ */
+public class Temporal {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
